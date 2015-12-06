e36d7d3cc5380cfa87019d8857916a25d42e83b0
Daniel Makurat <daniel.makurat@gmail.com>
2015-12-06 23:20:50 +0100

    remove Main.storyboard

--- Example/LocationPlayback/BIAppDelegate.m
+++ Example/LocationPlayback/BIAppDelegate.m
@@ -6,45 +6,43 @@
 //  Copyright (c) 2014 Daniel Makurat. All rights reserved.
 //
 
-#import "BIParseTripRepository.h"
 #import <LocationPlayback/BICloudRepositoryBuilder.h>
 #import <LocationPlayback/BILocationPlayback.h>
 #import <LocationPlayback/BILocationPlaybackConfiguration.h>
 #import "BIAppDelegate.h"
+#import "BIViewController.h"
 
 @implementation BIAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    BICloudRepositoryBuilder* repositoryBuilder = [[BICloudRepositoryBuilder alloc] initWithApplicationId:@"zwWNbqiQCuKo3hIKfnHPzIJfWgs2OjmyQSWijHlf" clientKey:@"yp7XjY1WvJnPRJNcg9ukAGODnqZUCeshLVCJvSQs"];
-    [[[BILocationPlayback instance] getConfiguration] setTripRepositoriesBuilder: repositoryBuilder];
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[BIViewController new]];
+    BICloudRepositoryBuilder *repositoryBuilder = [[BICloudRepositoryBuilder alloc] initWithApplicationId:@"zwWNbqiQCuKo3hIKfnHPzIJfWgs2OjmyQSWijHlf"
+                                                                                                clientKey:@"yp7XjY1WvJnPRJNcg9ukAGODnqZUCeshLVCJvSQs"];
+    [[[BILocationPlayback instance] getConfiguration] setTripRepositoriesBuilder:repositoryBuilder];
+    [self.window makeKeyAndVisible];
     return YES;
 }
-							
-- (void)applicationWillResignActive:(UIApplication *)application
-{
+
+- (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
