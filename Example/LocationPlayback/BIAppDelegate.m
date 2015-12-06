75f3041f94a5446616d0e781fd1686cd63e485c5
Daniel Makurat <daniel.makurat@gmail.com>
2015-12-06 23:20:50 +0100

    merge

--- Example/LocationPlayback/BIAppDelegate.m
+++ Example/LocationPlayback/BIAppDelegate.m
@@ -6,47 +6,44 @@
 //  Copyright (c) 2014 Daniel Makurat. All rights reserved.
 //
 
-#import "BIRegistryWithCloudRepository.h"
 #import <LocationPlayback/BITripPlayback.h>
 #import <LocationPlayback/BILocationPlaybackMainViewController.h>
 #import <LocationPlayback/BILocationPlayback.h>
 #import <LocationPlayback/BILocationPlaybackConfiguration.h>
 #import <LocationPlayback/BIRegistryWithCloudRepository.h>
 #import "BIAppDelegate.h"
+#import "BIViewController.h"
 
 @implementation BIAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[BIViewController new]];
     BIRegistryWithCloudRepository * repositoryBuilder = [[BIRegistryWithCloudRepository alloc] initWithApplicationId:@"zwWNbqiQCuKo3hIKfnHPzIJfWgs2OjmyQSWijHlf" clientKey:@"yp7XjY1WvJnPRJNcg9ukAGODnqZUCeshLVCJvSQs"];
     [[[BILocationPlayback instance] getConfiguration] setRegistry:repositoryBuilder];
+    [self.window makeKeyAndVisible];
     return YES;
 }
-							
-- (void)applicationWillResignActive:(UIApplication *)application
-{
+
+- (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
